55412c33b65346133cf529e6956423aec7f50944
satya483 <71950578+satya483@users.noreply.github.com>
2024-10-03 19:54:54 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessManagement;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BusinessManagement;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
